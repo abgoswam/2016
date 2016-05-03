d95a8079196cbcb220633c0ff3dbcdc145489df0
abgoswam <abgoswam@gmail.com>
2016-05-03 09:38:19 -0700

    fixing path

--- 032016/032016-master/web/WebServices/FormAppWebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace FormAppWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- 032016/032016-master/web/WebServices/FormAppWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FormAppWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
